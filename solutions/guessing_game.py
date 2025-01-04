d4f05ca7165078ed6b9c91640a0935193498573e
MohammadRAlSalloum <mohammadel.salloum1@gmail.com>
2025-01-04 21:06:50 +0200

    Add some documentation

--- solutions/guessing_game.py
+++ solutions/guessing_game.py
@@ -1,7 +1,7 @@
 """Guessing Game
 write a function that takes a random integer from the
 user (the player) and compare it to a randomly generated number inside
-the function.
+the function that is bound by user lower and upper boundaries.
 """
 
 import random
@@ -12,12 +12,25 @@ def guessing_game(lower_bound: int, upper_bound: int) -> int:
         generated integer from within the function.
 
     Parameters:
-        number: int, a random number generated within the function.
+        lower_bound: int, a number inserted by the player to specify the lower
+                        bound of the random integer.
+        upper_bound: int, a number inserted by the player to specify the upper
+                        bound of the random integer.               
 
     Returns -> str: You Won! (if the user's guess matched the random int)
             ->  str: Try Again (if the user's guess did not match the random int)
 
+    Raises:
+        AssertionError: if the lower_bound argument is not an int.
+        AssertionError: if the upper_bound argument is not an int.
+        
+    >>> guessing_game(2, 5)
+        You Won! (if the user's guess matched the random int)
+    >>> guessing_game(-3, 6) 
+        Try Again (if the user's guess did
+                    not match the random int)
 
+    
     """
 
 
